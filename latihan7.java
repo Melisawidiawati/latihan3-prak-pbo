7d0b6b126067c3f16d0f88477a422e688ceac7ba
Melisawidiawati <90242961+Melisawidiawati@users.noreply.github.com>
2021-10-07 11:44:46 +0800

    Add files via upload

---
+++ latihan7.java
@@ -0,0 +1,10 @@
+public class latihan7{
+	public static void main(String args[]){
+		int i;
+		for (i=1; i<9; i++)
+		{
+			System.out.println(i); //jika tulisan System.out.print tanpa "ln" maka hasil outputnya akan menjadi horizontal atau hasilnya akan menyambung terus ke kanan di baris yang sama
+			
+		}
+	}
+}
---
+++ latihan8.java
@@ -0,0 +1,9 @@
+public class latihan8 {
+	public static void main(String[] agrs){
+		int batas = 0;
+		while (batas<10){
+			System.out.print(batas);
+			batas++;  // jika tanda komentar di berikan pada batas++ maka outputnya akan muncul angka 0 yang terus terulang, dikarenakan angka 0 tidak akan mengalami increment, sehingga kondisi terminasinya tidak akan tercapai.
+		}
+	}
+}
